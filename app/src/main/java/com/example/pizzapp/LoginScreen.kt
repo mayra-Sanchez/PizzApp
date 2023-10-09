710d351bd880e1bae930e5bac943749aa8ec6079
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-10-09 16:07:19 -0500

    login UI ready

--- app/src/main/java/com/example/pizzapp/LoginScreen.kt
+++ app/src/main/java/com/example/pizzapp/LoginScreen.kt
@@ -1,7 +1,10 @@
 package com.example.pizzapp
 
 import android.content.Context
+import android.graphics.drawable.Icon
 import android.media.Image
+import android.util.Patterns
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
@@ -12,13 +15,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,9 +40,23 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.w3c.dom.Text
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.TextField
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun LoginScreen() {
     val context = LocalContext.current
@@ -48,33 +71,42 @@ fun LoginScreen() {
 
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color(0xFFF9EEC9))) {
+        .background(Color(236, 83, 76))) {
         Column(
             Modifier
                 .align(Alignment.Center)
                 .padding(16.dp)
                 .fillMaxWidth()) {
-            Card(
+            Box(
                 modifier = Modifier
                     .padding(12.dp)
-                    .background(Color(0xFFF9EEC9)),
-                shape = RoundedCornerShape(10.dp)
+                    .background(Color(249, 238, 201))
+                    .clip(RoundedCornerShape(10.dp)),
+
             ) {
-                Column(Modifier.padding(16.dp)) {
+                Column(
+                    Modifier
+                        .padding(16.dp)
+                        .align(Alignment.Center)) {
                     ImageLogin()
                     Email(
                         email = email,
                         emailChange = {
                             email = it
-                            isValidEmail = true
-                        }
+                            isValidEmail = Patterns.EMAIL_ADDRESS.matcher(email).matches()
+                        },
+                        isValidEmail
                     )
                     password(
                         password = password,
                         passwordChange = {
                             password = it
-                            isValidPassword = true
-                        }
+                            isValidPassword = password.length >= 6
+
+                        },
+                        passwordVisible = passwordVisible,
+                        passwordVisibleChange = { passwordVisible = !passwordVisible },
+                        isValidPassword = isValidPassword
 
                     )
                     buttonLogin(
@@ -82,61 +114,151 @@ fun LoginScreen() {
                         isValidEmail = isValidEmail,
                         isValidPassword = isValidPassword
                     )
+                    dontHaveAcount()
                 }
             }
         }
     }
 }
 
-
 @Composable
 fun ImageLogin(){
     Row(
-        Modifier
-            .fillMaxWidth()
-            .padding(10.dp),
         horizontalArrangement = Arrangement.Center) {
         Image(
-            modifier = Modifier.width(100.dp),
-            painter = painterResource(id = R.drawable.person),
+            modifier = Modifier.size(400.dp),
+            painter = painterResource(id = R.drawable.logologin),
             contentDescription = null,
         )
     }
+}
+
+@Composable
+fun dontHaveAcount(){
+    Row(
+        Modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center) {
+        Text(
+            text = "¿No tienes una cuenta?",
+            color = Color.Black,
+            modifier = Modifier
+                .padding(5.dp)
+        )
+
+        Text(
+            text = "Registrate",
+            color = Color.Blue,
+            modifier = Modifier
+                .padding(5.dp)
+        )
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Email(
     email:String,
-    emailChange: (String)->Unit
+    emailChange: (String)->Unit,
+    isValidEmail: Boolean
 ) {
     Row(
         Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(5.dp),
         horizontalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(value = email, onValueChange = emailChange)
+        OutlinedTextField(
+            shape = RoundedCornerShape(10.dp),
+            value = email,
+            onValueChange = emailChange,
+            label = {Text("Email")},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            maxLines = 1,
+            singleLine = true,
+            colors = if(isValidEmail){
+                     TextFieldDefaults.outlinedTextFieldColors(
+                         focusedLabelColor = Color.Green,
+                         focusedBorderColor = Color.Green)
+                     }else{
+                    TextFieldDefaults.outlinedTextFieldColors(
+                    focusedLabelColor = Color.Red,
+                    focusedBorderColor = Color.Red)
+                     },
+        )
     }
 }
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun password(
     password: String,
-    passwordChange: (String)->Unit
+    passwordChange: (String)->Unit,
+    passwordVisible: Boolean,
+    passwordVisibleChange: () -> Unit,
+    isValidPassword: Boolean
+
 ){
     Row(
         Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(5.dp),
         horizontalArrangement = Arrangement.Center
     ) {
-        OutlinedTextField(value = password, onValueChange = passwordChange)
+        OutlinedTextField(
+            visualTransformation = if(passwordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            shape = RoundedCornerShape(10.dp),
+            value = password,
+            onValueChange = passwordChange,
+            maxLines = 1,
+            singleLine = true,
+            label  = {Text("Contraseña")},
+            colors = if(isValidPassword){
+                TextFieldDefaults.outlinedTextFieldColors(
+                    focusedLabelColor = Color.Green,
+                    focusedBorderColor = Color.Green)
+            }else{
+                TextFieldDefaults.outlinedTextFieldColors(
+                    focusedLabelColor = Color.Red,
+                    focusedBorderColor = Color.Red)
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                PasswordVisibilityToggle(
+                    passwordVisible = passwordVisible,
+                    onPasswordVisibilityToggle = passwordVisibleChange
+                )
+            }
+        )
     }
-
 }
 
+@Composable
+fun PasswordVisibilityToggle(
+    passwordVisible: Boolean,
+    onPasswordVisibilityToggle: () -> Unit
+) {
+    val imageResId: Int = if (passwordVisible) {
+        R.drawable.visibility_off
+    } else {
+        R.drawable.visibility
+    }
+    IconButton(
+        onClick = onPasswordVisibilityToggle,
+        modifier = Modifier.padding(end = 8.dp)
+    ) {
+        Icon(
+            painter = painterResource(id = imageResId),
+            contentDescription = if (passwordVisible) "Ocultar contraseña" else "Mostrar contraseña"
+        )
+    }
+}
 @Composable
 fun buttonLogin(
     context: Context,
@@ -149,11 +271,17 @@ fun buttonLogin(
             .padding(10.dp),
         horizontalArrangement = Arrangement.Center) {
         Button(
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(116, 27, 15)
+            ),
             modifier = Modifier.fillMaxWidth(),
-            onClick = {},
-            enabled = isValidEmail && isValidPassword
+            onClick = {Login(context)},
+            //enabled = isValidEmail && isValidPassword
         ) {
-            Text(text = "Iniciar sesion")
+            Text(text = "Iniciar sesión")
         }
     }
+}
+fun Login(context: Context){
+    Toast.makeText(context, "Aca se hace la funcionalidad", Toast.LENGTH_LONG).show()
 }
