77a30f63bcca9a8d78f6c1472cbbcf95f8edca30
Lau36 <laura.jaimes@correounivalle.edu.co>
2023-11-25 21:51:27 -0500

    terms and conditions, signOut and delete my acount in the view

--- app/src/main/java/com/example/pizzapp/RegisterScreen.kt
+++ app/src/main/java/com/example/pizzapp/RegisterScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -19,10 +20,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.ui.text.input.KeyboardType
 
 @Composable
 fun RegisterScreen(navController: NavController) {
+    var isChecked by remember { mutableStateOf(false) }
+    var isDialogOpen by remember { mutableStateOf(false) }
+
     val context = LocalContext.current
 
     var email by remember { mutableStateOf("") }
@@ -86,6 +91,72 @@ fun RegisterScreen(navController: NavController) {
                         passwordVisibleChange = { passwordVisible = !passwordVisible },
                         isValidPassword = isValidPassword
                     )
+
+                    TermsAndConditions(
+                        isChecked = isChecked,
+                        onCheckedChange = { newCheckedState ->
+                            isChecked = newCheckedState
+                        },
+                        showDialog = { isDialogOpen = true } // Esta función abre el diálogo
+                    )
+
+                    TermsAndConditionsDialog(
+                        isOpen = isDialogOpen,
+                        onClose = { isDialogOpen = false }, // Esta función cierra el diálogo
+                        content = {
+                            // Aquí coloca el contenido de tus términos y condiciones
+                            Text("Términos y Condiciones de Uso - PizzApp\n" +
+                                    "\n" +
+                                    "Última actualización: 25/11/23\n" +
+                                    "\n" +
+                                    "Bienvenido a PizzApp, una aplicación para crear y compartir reseñas de pizzas. El acceso y uso de esta aplicación están sujetos a los siguientes términos y condiciones. Al utilizar PizzApp, aceptas estos términos en su totalidad.\n" +
+                                    "\n" +
+                                    "1. Registro de Usuarios\n" +
+                                    "\n" +
+                                    "1.1 Al registrarte en PizzApp, proporcionas información veraz, precisa y actualizada, incluyendo tu nombre, apellido, correo electrónico y una foto de perfil. Te comprometes a mantener actualizada toda la información proporcionada durante el registro.\n" +
+                                    "\n" +
+                                    "1.2 Te responsabilizas de mantener la confidencialidad de tu contraseña y de cualquier actividad que ocurra en tu cuenta.\n" +
+                                    "\n" +
+                                    "2. Privacidad\n" +
+                                    "\n" +
+                                    "2.1 Al utilizar PizzApp, aceptas nuestra Política de Privacidad, que describe cómo recopilamos, utilizamos y protegemos tu información personal. Al otorgarnos acceso a tu galería y cámara, aceptas el uso de estas funcionalidades para cargar fotos de perfil y contenido relacionado con las reseñas de pizzas.\n" +
+                                    "\n" +
+                                    "3. Uso de la Aplicación\n" +
+                                    "\n" +
+                                    "3.1 PizzApp es una plataforma para compartir reseñas de pizzas y explorar las opiniones de otros usuarios. Al crear reseñas, te comprometes a ofrecer opiniones honestas y precisas sobre las experiencias con las pizzas y las pizzerías mencionadas.\n" +
+                                    "\n" +
+                                    "3.2 Eres responsable del contenido que publicas en PizzApp. No debes enviar, publicar o compartir contenido que sea difamatorio, obsceno, ilegal o que infrinja los derechos de terceros.\n" +
+                                    "\n" +
+                                    "4. Propiedad Intelectual\n" +
+                                    "\n" +
+                                    "4.1 Todo el contenido proporcionado por PizzApp, incluyendo texto, gráficos, logotipos, imágenes, videos, y software, está protegido por derechos de autor y otras leyes de propiedad intelectual.\n" +
+                                    "\n" +
+                                    "4.2 Al utilizar PizzApp, no adquieres ningún derecho sobre la propiedad intelectual de la aplicación o su contenido. No puedes reproducir, distribuir, modificar o crear obras derivadas basadas en dicho contenido sin autorización previa.\n" +
+                                    "\n" +
+                                    "5. Modificaciones y Terminación\n" +
+                                    "\n" +
+                                    "5.1 Nos reservamos el derecho de modificar, suspender o interrumpir PizzApp en cualquier momento y por cualquier motivo sin previo aviso.\n" +
+                                    "\n" +
+                                    "5.2 Si incumples estos términos y condiciones, nos reservamos el derecho de terminar tu acceso a PizzApp sin previo aviso.\n" +
+                                    "\n" +
+                                    "6. Limitación de Responsabilidad\n" +
+                                    "\n" +
+                                    "6.1 PizzApp se proporciona \"tal cual\" y no ofrecemos garantías de ningún tipo con respecto a su precisión, confiabilidad o disponibilidad.\n" +
+                                    "\n" +
+                                    "6.2 No seremos responsables de ningún daño directo, indirecto, incidental, especial, consecuente o punitivo que surja del uso de PizzApp o la imposibilidad de usarlo.\n" +
+                                    "\n" +
+                                    "7. Ley Aplicable\n" +
+                                    "\n" +
+                                    "7.1 Estos términos y condiciones se rigen por las leyes del [país/estado] sin tener en cuenta sus conflictos de principios legales.\n" +
+                                    "\n" +
+                                    "Al utilizar PizzApp, aceptas estos términos y condiciones. Si no estás de acuerdo con alguno de estos términos, por favor, no utilices esta aplicación.\n" +
+                                    "\n" +
+                                    "Para preguntas o comentarios sobre estos términos y condiciones, contáctanos en PizzApp1226@gmail.com.\n" +
+                                    "\n" +
+                                    "¡Gracias por utilizar PizzApp!")
+                        }
+                    )
+
                     ButtonRegister(
                         context = context,
                         isValidEmail = isValidEmail,
@@ -95,7 +166,8 @@ fun RegisterScreen(navController: NavController) {
                         nombre = nombre,
                         apellido = apellido,
                         nombreUsuario = nombreUsuario,
-                        navController = navController
+                        navController = navController,
+                        isChecked = isChecked
                     )
                     YouHaveAccount(navController = navController)
                 }
@@ -191,6 +263,68 @@ fun NombreUsuario(
         )
     }
 }
+@Composable
+fun TermsAndConditions(
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+    showDialog: () -> Unit // Esta función abrirá el diálogo
+) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = isChecked,
+            onCheckedChange = { newCheckedState ->
+                onCheckedChange(newCheckedState)
+            },
+            colors = CheckboxDefaults.colors(
+                checkedColor = Color.Red
+            )
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = "Acepto los Términos y Condiciones",
+            color = Color.Black,
+            modifier = Modifier.clickable { showDialog() }
+        )
+    }
+}
+
+@Composable
+fun TermsAndConditionsDialog(
+    isOpen: Boolean,
+    onClose: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    if (isOpen) {
+        AlertDialog(
+            onDismissRequest = onClose,
+            title = {
+                Text(text = "Términos y Condiciones")
+            },
+            text = {
+                Box(modifier = Modifier.verticalScroll(rememberScrollState()).padding(8.dp).fillMaxWidth()){
+                    content()
+                }
+
+            },
+            confirmButton = {
+                Button(
+                    onClick = onClose,
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                )
+                {
+                    Text(text = "Aceptar")
+                }
+            },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
+
 
 @Composable
 fun YouHaveAccount(navController: NavController){
@@ -228,7 +362,8 @@ fun ButtonRegister(
     nombre: String,
     apellido: String,
     nombreUsuario: String,
-    navController: NavController
+    navController: NavController,
+    isChecked: Boolean
 
 ) {
     val firestoreRepository = FirestoreRepository()
@@ -243,16 +378,29 @@ fun ButtonRegister(
             colors = ButtonDefaults.buttonColors(containerColor = Color.Red),
             modifier = Modifier.fillMaxWidth(),
             onClick = {
-                if (isValidEmail && isValidPassword) {
-                    val user = User(nombre, apellido, email, nombreUsuario, password)
-                    firestoreRepository.createUser(user,
-                        onSuccess = {
-                            Toast.makeText(context, "Usuario registrado con éxito", Toast.LENGTH_LONG).show()
-                            navController.navigate("login")
-                        },
-                        onFailure = { e ->
-                            Toast.makeText(context, "Error al registrar: ${e.message}", Toast.LENGTH_LONG).show()
-                        })
+                if (isValidEmail && isValidPassword ){
+                    if(!isChecked){
+                        Toast.makeText(context, "Tienes que aceptar los términos y condiciones para hacer el registro", Toast.LENGTH_LONG).show()
+                    }
+                    else {
+                        val user = User(nombre, apellido, email, nombreUsuario, password)
+                        firestoreRepository.createUser(user,
+                            onSuccess = {
+                                Toast.makeText(
+                                    context,
+                                    "Usuario registrado con éxito",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                                navController.navigate("login")
+                            },
+                            onFailure = { e ->
+                                Toast.makeText(
+                                    context,
+                                    "Error al registrar: ${e.message}",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            })
+                    }
                 } else {
                     Toast.makeText(context, "Revisa los campos", Toast.LENGTH_LONG).show()
                 }
--- app/src/main/java/com/example/pizzapp/navbar/navbar.kt
+++ app/src/main/java/com/example/pizzapp/navbar/navbar.kt
@@ -12,12 +12,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
@@ -28,6 +34,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -37,12 +44,16 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.pizzapp.FirestoreRepository
 
+
 import com.example.pizzapp.R
 import com.example.pizzapp.User
 
 
 @Composable
 fun Navbar(navController: NavController) {
+    var isLogoutDialogOpen by remember { mutableStateOf(false) }
+    var isLogoutDialogOpen2 by remember { mutableStateOf(false) }
+
     val firestoreRepository = FirestoreRepository()
 
     // Obtener el usuario actual
@@ -118,16 +129,100 @@ fun Navbar(navController: NavController) {
                     modifier = Modifier
                         .padding(3.dp)
                         .background(Color(249, 238, 201))
-                ) {
+                )
+                {
                     DropdownMenuItem(text = { Text(text = "Mi perfil") }, leadingIcon = {Icon(Icons.Default.AccountCircle, contentDescription = "Mi perfil", tint = Color.Black)} , onClick = { navController.navigate("mi_perfil") } )
                     DropdownMenuItem(text = { Text(text = "Mis reseñas")}, leadingIcon = {Icon(Icons.Default.Create, contentDescription = "Mis reseñas", tint = Color.Black)} , onClick = { navController.navigate("mis_reseñas") })
-                    DropdownMenuItem(text = { Text(text = "Cerrar sesión")}, leadingIcon = {Icon(Icons.Default.Close, contentDescription = "Cerrar sesión", tint = Color.Black)}, onClick = { navController.navigate("inicio") })
+                    DropdownMenuItem(
+                        text = { Text(text = "Cerrar sesión") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.ExitToApp,
+                                contentDescription = "Cerrar sesión",
+                                tint = Color.Black
+                            )
+                        },
+                        onClick = {
+                            isLogoutDialogOpen = true
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Eliminar mi cuenta") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Close, contentDescription = "Eliminar cuenta", tint = Color.Black
+                            )},
+                        onClick = {
+                            isLogoutDialogOpen2 = true
+                        }
+                    )
                 }
             }
+
         }
     }
-}
+    if (isLogoutDialogOpen) {
+        AlertDialog(
+            onDismissRequest = { isLogoutDialogOpen = false },
+            title = { Text("Cerrar sesión") },
+            text = { Text("¿Estás seguro que desea cerrar sesión?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        //Acá se llamaria al servicio de cerrar sesion
+                        navController.navigate("inicio")
+                        isLogoutDialogOpen = false
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text("Confirmar")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        isLogoutDialogOpen = false
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text("Cancelar")
+                }
+            },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+    if (isLogoutDialogOpen2) {
+        AlertDialog(
+            onDismissRequest = { isLogoutDialogOpen2 = false },
+            title = { Text("Eliminar cuenta") },
+            text = { Text("¿Estás seguro que desea eliminar su cuenta?") },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        //Acá se llamaria al servicio de eliminar cuenta
+                        navController.navigate("inicio")
+                        isLogoutDialogOpen2 = false
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text("Confirmar")
 
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = {
+                        isLogoutDialogOpen2 = false
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+                ) {
+                    Text("Cancelar")
+                }
+            },
+            modifier = Modifier.padding(16.dp)
+        )
+    }
+}
 
 @Composable
 fun ImageInitial(navController: NavController){
